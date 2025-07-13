b06094530b1b8051e4f0a55b3acb4b73c99ee258
Imama Ansari <ansariimama@yahoo.com>
2025-07-14 01:54:51 +0500

    Some changes in UI

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
 const supabaseUrl = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNyc3VwZ2JqdWt4aWZ5Zm1kY2VwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE0Mzg3ODUsImV4cCI6MjA2NzAxNDc4NX0.-yMtzbLPOU_R10ysCz1-sDwl2B4dkvWMU7--R5Zy9Wg"
 const supabaseKey = "srsupgbjukxifyfmdcep"
+const { createClient } = supabase;
